f4edc73a7e314f3992831abcd57f8beb355a23b7
Aleksandr Korobeinikov <alexandr.korobeinikov@emotorwerks.com>
2021-07-20 11:45:44 +0300

    lesson 3

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 
 	"github.com/Korablik/Go.Websocket/internal"
+	"github.com/Korablik/Go.Websocket/internal/handlers"
 )
 
 func main() {
 	mux := internal.Routes()
 
+	log.Println("Starting channel listener")
+
+	go handlers.ListenToWsChannel()
+
 	log.Println("Starting web server on port 8080")
 
 	_ = http.ListenAndServe(":8080", mux)
+	//_ = http.ListenAndServeTLS(":8083", mux)
 }
